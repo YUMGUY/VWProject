4d0fe611af19bda0febeaae356424e3aa0aa812b
YUMGUY <81205938+YUMGUY@users.noreply.github.com>
2026-06-26 23:23:42 -0500

    Test commit

---
+++ Assets/DialogueSystem/TestScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public GameObject testPrefab = null;
+
+    private void Start()
+    {
+        print("Hello world");
+    }
+
+    private void Update()
+    {
+        
+    }
+}
